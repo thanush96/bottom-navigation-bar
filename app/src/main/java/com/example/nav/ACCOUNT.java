71b50dd921d60000c8915bf1460b19c3bc312a8d
thanush96 <danushthanush@gmail>
2020-12-09 19:42:51 +0530

    Intent add into fragment

--- app/src/main/java/com/example/nav/ACCOUNT.java
+++ app/src/main/java/com/example/nav/ACCOUNT.java
@@ -1,10 +1,12 @@
 package com.example.nav;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.fragment.app.Fragment;
 
@@ -14,17 +16,33 @@ import androidx.fragment.app.Fragment;
  */
 public class ACCOUNT extends Fragment {
 
+    Button button;
+
 
     public ACCOUNT() {
         // Required empty public constructor
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.account, container, false);
+
+
+        View view= inflater.inflate(R.layout.account, container, false);
+        button= view.findViewById(R.id.btn);
+
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent=new Intent(getActivity(),TWO.class);
+                startActivity(intent);
+
+            }
+        });
+        return view;
     }
 
 }
